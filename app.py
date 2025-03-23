ecd994f199b631c7866ee9f736edf3236bc28464
hanzalah57 <hanzalahsohail57@gmail.com>
2025-03-23 12:29:47 +0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd
-from sklearn.preprocessing import StandardScaler,LabelEncoder, OneHotEncoder
 import pickle
 import tensorflow as tf
 
